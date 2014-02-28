fe55630b7723f9b3a89f3ac58489581ce3701c9d
SR1s <seemsnevermind@gmail.com>
2014-02-28 21:00:53 +0800

    add init function for note class

--- models/Note.py
+++ models/Note.py
@@ -1,9 +1,19 @@
 from myreadings.models import db
+from datetime import datetime
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
     user_id = db.Column(db.Integer)
     item_id = db.Column(db.Integer)
-    add_date = db.Column(db.DateTime)
-    last_edit = db.Column(db.DateTime) 
+    add_date = db.Column(db.DateTime, default=datetime.utcnow)
+    last_edit = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, content, user_id, item_id):
+        self.content = content
+        self.user_id = user_id
+        self.item_id = item_id
+
+    def __repr__(Note):
+        return '<Item: %s By %s - %s>' % \
+                (self.content, self.item_id, self.add_date)
