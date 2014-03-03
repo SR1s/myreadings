96c085149a12025f9b3a758d38d5821a4d74108d
SR1s <seemsnevermind@gmail.com>
2014-03-04 01:58:37 +0800

    add local_config file

---
+++ myreadings/config/local_config.py
@@ -0,0 +1,7 @@
+# local config
+MYSQL_HOST   = "database host" # your database host
+MYSQL_HOST_S = ""
+MYSQL_PORT   = "3306"          # database port, default 3306 
+MYSQL_USER   = "username"      # database username
+MYSQL_PASS   = "password"      # database password
+MYSQL_DB     = "database name" # database name
