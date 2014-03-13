2ccd1abb6fa55dfd73a1fc827fe410fd7896c278
SR1s <seemsnevermind@gmail.com>
2014-03-13 23:39:25 +0800

    change model: username no need to be unique and add init function for model

--- myreadings/models/User.py
+++ myreadings/models/User.py
@@ -1,8 +1,16 @@
 from myreadings.models import db
+from 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True)
+    username = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     sina_id = db.Column(db.String(255))
+
+    def __init__(self, username, email, password, sina_id=None):
+    	self.username = username
+    	self.email = email
+    	self.password = password
+    	if sina_id:
+    		self.sina_id = sina_id
