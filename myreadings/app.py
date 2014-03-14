392796e5557c3c91faa73296db219b69c9766273
SR1s <seemsnevermind@gmail.com>
2014-03-14 22:32:24 +0800

    add userinfo function for tempaltes and change logic to fit changes

--- myreadings/app.py
+++ myreadings/app.py
@@ -3,7 +3,8 @@ import sys, os
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
-from flask import Flask, g, request, render_template, flash, redirect, url_for
+from flask import Flask, g, request, render_template, flash, redirect, url_for, \
+                  session
 
 from myreadings.models import Item, Note, User, db
 
@@ -22,13 +23,23 @@ app.secret_key = 'guesswhatkeyitis'
 db.init_app(app)
 app.register_blueprint(user, url_prefix="/user")
 
+@app.context_processor
+def jinja2():
+    def user_info():
+        if session.get('status', None) == 'logined' and session['user_id']:
+            return User.query.filter_by(id=session['user_id']).first()
+        return None
+    return dict(user_info=user_info)
+
 @app.route('/')
 def index():
     notes = Note.query.order_by(Note.add_date.desc()).all()
     items = list()
     for note in notes:
         item = Item.query.filter_by(id=note.item_id).first()
+        user = User.query.filter_by(id=note.user_id).first()
         note.mixinItem(item)
+        note.mixinUser(user)
         items.append(note)
     return render_template("index.html", items=items)
 
@@ -43,10 +54,12 @@ def add():
         db.session.add(item)
         db.session.commit()
 
-        note = Note(description, 1, item.id)
+        user = User.query.filter_by(id = session['user_id']).first()
+
+        note = Note(description, user.id, item.id)
         db.session.add(note)
         db.session.commit()
-        return redirect(url_for("hello"))
+        return redirect(url_for("index"))
     return render_template("add.html")
 
 @app.route('/u/<int:user_id>')
